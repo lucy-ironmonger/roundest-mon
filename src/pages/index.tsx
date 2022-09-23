f53db2c4366d2dc4ed18f36183c22ba6f31aa896
Lucy Ironmonger <37961811+ironmongrrrl@users.noreply.github.com>
2022-09-23 20:47:15 +0100

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,54 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { trpc } from "../utils/trpc";
+
+const Home: NextPage = () => {
+  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
+
+  return (
+    <>
+      <Head>
+        <title>Roundest Mon</title>
+        <meta name="description" content="The roundest pokemon!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
+        <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
+          Roundest <span className="text-purple-300">Pokemon</span> App
+        </h1>
+        <p className="text-2xl text-gray-700">This stack uses:</p>
+        <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3"></div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
+
+// type TechnologyCardProps = {
+//   name: string;
+//   description: string;
+//   documentation: string;
+// };
+
+// const TechnologyCard = ({
+//   name,
+//   description,
+//   documentation,
+// }: TechnologyCardProps) => {
+//   return (
+//     <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
+//       <h2 className="text-lg text-gray-700">{name}</h2>
+//       <p className="text-sm text-gray-600">{description}</p>
+//       <a
+//         className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
+//         href={documentation}
+//         target="_blank"
+//         rel="noreferrer"
+//       >
+//         Documentation
+//       </a>
+//     </section>
+//   );
+// };
