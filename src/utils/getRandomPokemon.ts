775a9c3395e24430271f68e112c1cc7b27048e22
Lucy Ironmonger <37961811+ironmongrrrl@users.noreply.github.com>
2022-09-23 23:56:24 +0100

    adding return type for getRandomPokemon

--- src/utils/getRandomPokemon.ts
+++ src/utils/getRandomPokemon.ts
@@ -1,6 +1,8 @@
 const MAX_DEX_ID = 493;
 
-export const getRandomPokemon = (notThisOne?: number) => {
+export const getRandomPokemon: (notThisOne?: number) => number = (
+  notThisOne
+) => {
   const pokedexNumber = Math.floor(Math.random() * (MAX_DEX_ID - 1) + 1);
   // Math.random = number between 0 and 1
   // Times up to 492
@@ -9,5 +11,9 @@ export const getRandomPokemon = (notThisOne?: number) => {
   // 0 x 493 = 0 + 1 = 1
 
   if (pokedexNumber !== notThisOne) return pokedexNumber;
-  getRandomPokemon(notThisOne);
+  return getRandomPokemon(notThisOne);
 };
+
+// when adding return - getRandomPokemon' implicitly has return type 'any'
+// because it does not have a return type annotation
+// and is referenced directly or indirectly in one of its return expressions
