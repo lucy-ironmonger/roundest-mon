677ac6ce6b6996508403fde6e34c4d1a7d4864cc
Lucy Ironmonger <37961811+ironmongrrrl@users.noreply.github.com>
2022-09-24 23:20:36 +0100

    ssr false now getting pokemon through

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,15 +8,29 @@ export default function Home() {
   const [first, second] = ids;
 
   const firstPokemon = trpc.useQuery(["get-pokemon-by-id", { id: first }]);
+  const secondPokemon = trpc.useQuery(["get-pokemon-by-id", { id: second }]);
+
+  if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
+
   console.log(firstPokemon.data);
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokémon is Rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-800">{first}</div>
+        <div className="w-64 h-64 bg-red-800">
+          <img
+            src={firstPokemon.data?.sprites.front_default}
+            className="w-full"
+          ></img>
+        </div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-800">{second}</div>
+        <div className="w-64 h-64 bg-red-800">
+          <img
+            src={secondPokemon.data?.sprites.front_default}
+            className="w-full"
+          ></img>
+        </div>
       </div>
     </div>
   );
