32117eaacf4876456a33ff69455b6efe4fc6e548
Lucy Ironmonger <37961811+ironmongrrrl@users.noreply.github.com>
2022-09-25 22:13:44 +0100

    inferQueryResponse PokemonListing comp w/ React.FC

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,4 +1,5 @@
-import { appRouter } from "@/backend/router";
+import { AppRouter, appRouter } from "@/backend/router";
+import { inferProcedureOutput } from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 
 // export API handler
@@ -6,3 +7,8 @@ export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext: () => null,
 });
+
+// helper - gives an infer helper that yanks out of AppRouter the type of a specific thing that we are doing
+export type inferQueryResponse<
+  TRouteKey extends keyof AppRouter["_def"]["queries"]
+> = inferProcedureOutput<AppRouter["_def"]["queries"][TRouteKey]>;
